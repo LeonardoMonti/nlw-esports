79be5322483b34b1c312ebfcb104e8346a9e42d2
LeonardoMonti <LeonardoMonti>
2022-09-20 17:08:29 -0300

    fix: change component for arrow function

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -4,26 +4,25 @@ interface GameBannerProps {
   adsCount: number;
 }
 
-export function GameBanner({
+export const GameBanner: React.FC<GameBannerProps> = ({
   adsCount,
   title,
   bannerUrl,
-}: GameBannerProps) {
-  return (
-    <a 
-      href="#" 
-      className="keen-slider__slide relative overflow-hidden rounded-lg shadow-md transition-transform hover:scale-105"
-    >
-    <img src={bannerUrl} alt={`image de ${title}`} />
+}) => (
+  <a
+    href="#"
+    className="keen-slider__slide relative overflow-hidden rounded-lg shadow-md transition-transform hover:scale-105"
+  >
+    <img src={bannerUrl} alt={`imagem de ${title}`} />
 
     <div className="bg-game-gradient absolute inset-x-0 bottom-0 w-full px-4 pt-16 pb-4">
       <strong className="block text-sm font-bold text-white sm:text-base">
         {title}
       </strong>
+
       <span className="mt-1 text-xs text-zinc-300 sm:text-sm">
         {adsCount} anúncio{adsCount !== 1 && "s"}
       </span>
     </div>
   </a>
-  )
-}
+);
